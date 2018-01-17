ec203c8dd9905c42eff3e4ec33dd167304579512
josepablito16 <josepablocif16@gmail.com>
2018-01-16 22:13:05 -0600

    Se agrego la interfaz y 3 metodos

---
+++ HT1Estructuras/src/Radio.java
@@ -0,0 +1,102 @@
+
+import java.text.DecimalFormat;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose
+ */
+public class Radio implements douglas
+{
+ 
+    
+    @Override
+    public void onOff(boolean e) 
+    {
+       
+        //jose
+        
+    }
+
+    @Override
+    public void Switch(boolean e) 
+    {
+               
+        
+        //jose
+    }
+
+    @Override
+    public float siguiente(float a) 
+    {
+       
+        if (a%10==0) 
+        {
+            //es AM
+            
+                if (a==1610) 
+                {
+                    a=530;
+                    return a;
+                    
+                }
+                else
+                {
+                    a+=10;
+                }               
+                
+            
+            
+            
+        }
+        else
+        {
+            //es FM
+            
+                if (a>107.9) 
+                {
+                    a=(float) 87.9;
+                    
+                    return (float) 87.9;
+                    
+                }
+                else
+                {
+                    a+=0.2;
+                }
+                
+                
+            
+            
+            
+        }
+          
+        
+        
+        //jose
+        return a;
+    }
+
+    @Override
+    public float anterior(float a) 
+    {
+        return 20;
+    }
+
+    @Override
+    public void guardar(float e, int b) 
+    {
+    }
+
+    @Override
+    public float seleccionarFav(int b) 
+    {
+        return 20;
+    }
+    
+}
---
+++ HT1Estructuras/src/douglas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose
+ */
+public interface douglas 
+{
+    public void onOff(boolean e);
+    public void Switch(boolean e);
+    public float siguiente(float a);
+    public float anterior(float a);
+    public void guardar(float e, int b);
+    public float seleccionarFav(int b);
+   
+    
+}
